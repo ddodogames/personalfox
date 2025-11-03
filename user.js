b7d509171a3d3879b9b354df1099dce39cab9877
dodoGames <69091682+ddodogames@users.noreply.github.com>
2025-11-03 23:58:32 +0200

    don't suggest searches and engines

--- user.js
+++ user.js
@@ -6,6 +6,7 @@ user_pref("app.normandy.first_run", false);
 // Some tweaks
 user_pref("browser.aboutConfig.showWarning", false);
 user_pref("browser.discovery.enabled", false);
+user_pref("browser.discovery.containers.enabled", false);
 user_pref("browser.ml.chat.enabled", false);
 user_pref("browser.ml.enable", false);
 user_pref("extensions.ml.enabled", false);
@@ -19,7 +20,8 @@ user_pref("app.shield.optoutstudies.enabled", true);
 
 // Extra stuff for myself
 user_pref("dom.ipc.processCount", 3);
-user_pref("browser.discovery.containers.enabled", false);
 user_pref("general.smoothScroll", false);
 user_pref("browser.aboutwelcome.enabled", false);
 user_pref("browser.tabs.unloadOnLowMemory", true);
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.searches", false);
