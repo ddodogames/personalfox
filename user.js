ceb0a8e0d76137c9dd58e9ddad06c5ff07674b7c
dodoGames <69091682+ddodogames@users.noreply.github.com>
2025-11-03 23:52:44 +0200

    initial version

---
+++ user.js
@@ -0,0 +1,25 @@
+// Get rid of normady
+user_pref("app.normandy.api_url", "");
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.first_run", false);
+
+// Some tweaks
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.ml.chat.enabled", false);
+user_pref("browser.ml.enable", false);
+user_pref("extensions.ml.enabled", false);
+user_pref("browser.formfill.enable", false);
+user_pref("browser.urlbar.trimURLs", false);
+user_pref("browser.urlbar.trending.featureGate", false);
+user_pref("browser.uitour.enabled", false);
+user_pref("browser.urlbar.wikipedia.featureGate", false);
+user_pref("browser.urlbar.amp.featureGate", false);
+user_pref("app.shield.optoutstudies.enabled", true);
+
+// Extra stuff for myself
+user_pref("dom.ipc.processCount", 3);
+user_pref("browser.discovery.containers.enabled", false);
+user_pref("general.smoothScroll", false);
+user_pref("browser.aboutwelcome.enabled", false);
+user_pref("browser.tabs.unloadOnLowMemory", true);
